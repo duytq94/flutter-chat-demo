31af2defe003d728a6f252fbe131b307340ca320
Duy Tran <tranquangduy7994@gmail.com>
2018-08-15 15:26:52 +0700

    update

---
+++ lib/login.dart
@@ -0,0 +1,87 @@
+import 'dart:async';
+import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter_chat_demo/main.dart';
+import 'package:google_sign_in/google_sign_in.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:fluttertoast/fluttertoast.dart';
+
+void main() => runApp(new MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return new MaterialApp(
+      title: 'Chat Demo',
+      theme: new ThemeData(
+        primaryColor: new Color(0xfff5a623),
+      ),
+      home: new LoginScreen(title: 'CHAT DEMO'),
+      debugShowCheckedModeBanner: false,
+      routes: <String, WidgetBuilder>{
+        '/main': (_) => new Main(), // Home Page
+      },
+    );
+  }
+}
+
+class LoginScreen extends StatefulWidget {
+  LoginScreen({Key key, this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  LoginScreenState createState() => new LoginScreenState();
+}
+
+class LoginScreenState extends State<LoginScreen> {
+  final GoogleSignIn googleSignIn = new GoogleSignIn();
+  final FirebaseAuth firebaseAuth = FirebaseAuth.instance;
+
+  Future<Null> handleSignIn() async {
+    GoogleSignInAccount googleUser = await googleSignIn.signIn();
+    GoogleSignInAuthentication googleAuth = await googleUser.authentication;
+    FirebaseUser firebaseUser = await firebaseAuth.signInWithGoogle(
+      accessToken: googleAuth.accessToken,
+      idToken: googleAuth.idToken,
+    );
+    if (firebaseUser != null) {
+      Firestore.instance
+          .collection('users')
+          .document(firebaseUser.uid)
+          .setData({'displayName': firebaseUser.displayName, 'photoUrl': firebaseUser.photoUrl});
+      Fluttertoast.showToast(msg: "Sign in success");
+
+      // Navigate and reset route
+      Navigator.of(context).pushNamedAndRemoveUntil('/main', (Route<dynamic> route) => false);
+    } else {
+      Fluttertoast.showToast(msg: "Sign in fail");
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+      appBar: new AppBar(
+        title: new Text(
+          widget.title,
+          style: new TextStyle(color: new Color(0xff203152), fontWeight: FontWeight.bold),
+        ),
+        centerTitle: true,
+        backgroundColor: new Color(0xfff5a623),
+      ),
+      body: new Center(
+        child: new FlatButton(
+            onPressed: handleSignIn,
+            child: new Text(
+              'SIGN IN WITH GOOGLE',
+              style: new TextStyle(fontSize: 16.0),
+            ),
+            color: new Color(0xffdd4b39),
+            highlightColor: new Color(0xffff7f7f),
+            textColor: Colors.white,
+            padding: new EdgeInsets.fromLTRB(30.0, 15.0, 30.0, 15.0)),
+      ),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,71 +1,121 @@
 import 'dart:async';
-
+import 'dart:io';
 import 'package:flutter/material.dart';
-import 'package:firebase_auth/firebase_auth.dart';
-import 'package:google_sign_in/google_sign_in.dart';
 
-void main() => runApp(new MyApp());
-
-class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    return new MaterialApp(
-        title: 'Chat Demo',
-        theme: new ThemeData(
-          primarySwatch: Colors.amber,
-        ),
-        home: new MyHomePage(title: 'CHAT DEMO'),
-        debugShowCheckedModeBanner: false);
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  final String title;
-
-  @override
-  _MyHomePageState createState() => new _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  final GoogleSignIn _googleSignIn = new GoogleSignIn();
-  final FirebaseAuth _firebaseAuth = FirebaseAuth.instance;
-
-  Future<FirebaseUser> _handleSignIn() async {
-    GoogleSignInAccount googleUser = await _googleSignIn.signIn();
-    GoogleSignInAuthentication googleAuth = await googleUser.authentication;
-    FirebaseUser firebaseUser = await _firebaseAuth.signInWithGoogle(
-      accessToken: googleAuth.accessToken,
-      idToken: googleAuth.idToken,
-    );
-    return firebaseUser;
-  }
-
+class Main extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return new Scaffold(
       appBar: new AppBar(
         title: new Text(
-          widget.title,
-          style: new TextStyle(color: new Color(0xff203152), fontWeight: FontWeight.bold),
+          'MAIN',
+          style: new TextStyle(fontSize: 18.0, fontWeight: FontWeight.bold),
         ),
         centerTitle: true,
-        backgroundColor: new Color(0xfff5a623),
       ),
-      body: new Center(
-        child: new FlatButton(
-            onPressed: () => _handleSignIn().then((value) => print(value)),
-            child: new Text(
-              'SIGN IN WITH GOOGLE',
-              style: new TextStyle(fontSize: 16.0),
-            ),
-            color: new Color(0xffdd4b39),
-            highlightColor: new Color(0xffff7f7f),
-            textColor: Colors.white,
-            padding: new EdgeInsets.fromLTRB(30.0, 15.0, 30.0, 15.0)),
-      ),
+      body: new MainScreen(),
     );
+  }
+}
+
+class MainScreen extends StatefulWidget {
+  @override
+  State createState() => new MainScreenState();
+}
+
+class MainScreenState extends State<MainScreen> {
+  bool isLoading = false;
+
+  Future<bool> onWillPopScope() {
+    openDialog();
+    return new Future.value(false);
+  }
+
+  Future<Null> openDialog() async {
+    switch (await showDialog(
+        context: context,
+        builder: (BuildContext context) {
+          return new SimpleDialog(
+            contentPadding: new EdgeInsets.only(left: 0.0, right: 0.0, top: 0.0, bottom: 10.0),
+            children: <Widget>[
+              new Container(
+                color: new Color(0xfff5a623),
+                margin: new EdgeInsets.all(0.0),
+                padding: new EdgeInsets.only(bottom: 10.0, top: 10.0),
+                child: new Column(
+                  children: <Widget>[
+                    new Container(
+                      child: new Icon(
+                        Icons.exit_to_app,
+                        size: 30.0,
+                        color: Colors.white,
+                      ),
+                      margin: new EdgeInsets.only(bottom: 10.0),
+                    ),
+                    new Text(
+                      'Exit app',
+                      style: new TextStyle(color: Colors.white, fontSize: 18.0, fontWeight: FontWeight.bold),
+                    ),
+                    new Text(
+                      'Are you sure to exit app?',
+                      style: new TextStyle(color: Colors.white70, fontSize: 14.0),
+                    ),
+                  ],
+                ),
+              ),
+              new SimpleDialogOption(
+                onPressed: () {
+                  Navigator.pop(context, 0);
+                },
+                child: new Row(
+                  children: <Widget>[
+                    new Container(
+                      child: new Icon(
+                        Icons.cancel,
+                        color: new Color(0xff203152),
+                      ),
+                      margin: new EdgeInsets.only(right: 20.0),
+                    ),
+                    new Text(
+                      'CANCEL',
+                      style: new TextStyle(color: new Color(0xff203152), fontWeight: FontWeight.bold),
+                    )
+                  ],
+                ),
+              ),
+              new SimpleDialogOption(
+                onPressed: () {
+                  Navigator.pop(context, 1);
+                },
+                child: new Row(
+                  children: <Widget>[
+                    new Container(
+                      child: new Icon(
+                        Icons.check_circle,
+                        color: new Color(0xff203152),
+                      ),
+                      margin: new EdgeInsets.only(right: 20.0),
+                    ),
+                    new Text(
+                      'YES',
+                      style: new TextStyle(color: new Color(0xff203152), fontWeight: FontWeight.bold),
+                    )
+                  ],
+                ),
+              ),
+            ],
+          );
+        })) {
+      case 0:
+        break;
+      case 1:
+        exit(0);
+        break;
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return new WillPopScope(child: new Center(), onWillPop: onWillPopScope);
   }
 }
