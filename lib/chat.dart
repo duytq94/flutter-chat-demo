1b8a0a532eb0d407bfd7ab363e4e705875da622b
Duy Tran <tranquangduy7994@gmail.com>
2018-08-22 15:17:50 +0700

    add date time message

--- lib/chat.dart
+++ lib/chat.dart
@@ -11,6 +11,7 @@ import 'package:fluttertoast/fluttertoast.dart';
 import 'package:image_picker/image_picker.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:cached_network_image/cached_network_image.dart';
+import 'package:intl/intl.dart';
 
 class Chat extends StatelessWidget {
   final String peerId;
@@ -53,6 +54,7 @@ class ChatScreenState extends State<ChatScreen> {
   String peerAvatar;
   String id;
 
+  var listMessage;
   String groupChatId;
   SharedPreferences prefs;
 
@@ -161,10 +163,9 @@ class ChatScreenState extends State<ChatScreen> {
                   padding: EdgeInsets.fromLTRB(15.0, 10.0, 15.0, 10.0),
                   width: 200.0,
                   decoration: BoxDecoration(color: greyColor2, borderRadius: BorderRadius.circular(8.0)),
-                  margin: EdgeInsets.only(bottom: 15.0, right: 10.0),
+                  margin: EdgeInsets.only(bottom: isLastMessageRight(index) ? 20.0 : 10.0, right: 10.0),
                 )
               : Container(
-                  margin: EdgeInsets.only(bottom: 15.0, right: 10.0),
                   child: Material(
                     child: CachedNetworkImage(
                       placeholder: Container(
@@ -182,6 +183,7 @@ class ChatScreenState extends State<ChatScreen> {
                     ),
                     borderRadius: BorderRadius.all(Radius.circular(8.0)),
                   ),
+                  margin: EdgeInsets.only(bottom: isLastMessageRight(index) ? 20.0 : 10.0, right: 10.0),
                 ),
         ],
         mainAxisAlignment: MainAxisAlignment.end,
@@ -189,63 +191,98 @@ class ChatScreenState extends State<ChatScreen> {
     } else {
       // Left (peer message)
       return Container(
-        child: Row(
+        child: Column(
           children: <Widget>[
-            Material(
-              child: CachedNetworkImage(
-                placeholder: Container(
-                  child: CircularProgressIndicator(
-                    strokeWidth: 1.0,
-                    valueColor: AlwaysStoppedAnimation<Color>(themeColor),
-                  ),
-                  width: 35.0,
-                  height: 35.0,
-                  padding: EdgeInsets.all(10.0),
-                ),
-                imageUrl: peerAvatar,
-                width: 35.0,
-                height: 35.0,
-                fit: BoxFit.cover,
-              ),
-              borderRadius: BorderRadius.all(
-                Radius.circular(18.0),
-              ),
+            Row(
+              children: <Widget>[
+                isLastMessageLeft(index)
+                    ? Material(
+                        child: CachedNetworkImage(
+                          placeholder: Container(
+                            child: CircularProgressIndicator(
+                              strokeWidth: 1.0,
+                              valueColor: AlwaysStoppedAnimation<Color>(themeColor),
+                            ),
+                            width: 35.0,
+                            height: 35.0,
+                            padding: EdgeInsets.all(10.0),
+                          ),
+                          imageUrl: peerAvatar,
+                          width: 35.0,
+                          height: 35.0,
+                          fit: BoxFit.cover,
+                        ),
+                        borderRadius: BorderRadius.all(
+                          Radius.circular(18.0),
+                        ),
+                      )
+                    : Container(width: 35.0),
+                document['type'] == 0
+                    ? Container(
+                        child: Text(
+                          document['content'],
+                          style: TextStyle(color: Colors.white),
+                        ),
+                        padding: EdgeInsets.fromLTRB(15.0, 10.0, 15.0, 10.0),
+                        width: 200.0,
+                        decoration: BoxDecoration(color: primaryColor, borderRadius: BorderRadius.circular(8.0)),
+                        margin: EdgeInsets.only(left: 10.0),
+                      )
+                    : Container(
+                        child: Material(
+                          child: CachedNetworkImage(
+                            placeholder: Container(
+                              child: CircularProgressIndicator(
+                                valueColor: AlwaysStoppedAnimation<Color>(themeColor),
+                              ),
+                              width: 200.0,
+                              height: 200.0,
+                              padding: EdgeInsets.all(70.0),
+                            ),
+                            imageUrl: document['content'],
+                            width: 200.0,
+                            height: 200.0,
+                            fit: BoxFit.cover,
+                          ),
+                          borderRadius: BorderRadius.all(Radius.circular(8.0)),
+                        ),
+                        margin: EdgeInsets.only(left: 10.0),
+                      ),
+              ],
             ),
-            document['type'] == 0
+
+            // Time
+            isLastMessageLeft(index)
                 ? Container(
                     child: Text(
-                      document['content'],
-                      style: TextStyle(color: Colors.white),
+                      DateFormat('dd MMM kk:mm')
+                          .format(DateTime.fromMillisecondsSinceEpoch(int.parse(document['timestamp']))),
+                      style: TextStyle(color: greyColor, fontSize: 12.0, fontStyle: FontStyle.italic),
                     ),
-                    padding: EdgeInsets.fromLTRB(15.0, 10.0, 15.0, 10.0),
-                    width: 200.0,
-                    decoration: BoxDecoration(color: primaryColor, borderRadius: BorderRadius.circular(8.0)),
-                    margin: EdgeInsets.only(left: 10.0),
+                    margin: EdgeInsets.only(left: 50.0, top: 5.0, bottom: 5.0),
                   )
-                : Container(
-                    child: Material(
-                      child: CachedNetworkImage(
-                        placeholder: Container(
-                          child: CircularProgressIndicator(
-                            valueColor: AlwaysStoppedAnimation<Color>(themeColor),
-                          ),
-                          width: 200.0,
-                          height: 200.0,
-                          padding: EdgeInsets.all(70.0),
-                        ),
-                        imageUrl: document['content'],
-                        width: 200.0,
-                        height: 200.0,
-                        fit: BoxFit.cover,
-                      ),
-                      borderRadius: BorderRadius.all(Radius.circular(8.0)),
-                    ),
-                    margin: EdgeInsets.only(left: 10.0),
-                  ),
+                : Container()
           ],
+          crossAxisAlignment: CrossAxisAlignment.start,
         ),
-        margin: EdgeInsets.only(bottom: 15.0),
+        margin: EdgeInsets.only(bottom: 10.0),
       );
+    }
+  }
+
+  bool isLastMessageLeft(int index) {
+    if ((index > 0 && listMessage != null && listMessage[index - 1]['idFrom'] == id) || index == 0) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  bool isLastMessageRight(int index) {
+    if ((index > 0 && listMessage != null && listMessage[index - 1]['idFrom'] != id) || index == 0) {
+      return true;
+    } else {
+      return false;
     }
   }
 
@@ -272,6 +309,7 @@ class ChatScreenState extends State<ChatScreen> {
                           return Center(
                               child: CircularProgressIndicator(valueColor: AlwaysStoppedAnimation<Color>(themeColor)));
                         } else {
+                          listMessage = snapshot.data.documents;
                           return ListView.builder(
                             padding: EdgeInsets.all(10.0),
                             itemBuilder: (context, index) => buildItem(index, snapshot.data.documents[index]),
