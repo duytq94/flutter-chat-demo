ee43410f75a7f55cc851c4e1003fc436b884b04d
Duy Tran <tranquangduy7994@gmail.com>
2018-08-31 14:41:32 +0700

    fix bug auto update when sign out

--- lib/login.dart
+++ lib/login.dart
@@ -50,75 +50,78 @@ class LoginScreenState extends State<LoginScreen> {
   }
 
   void isSignedIn() async {
-    isLoggedIn = await googleSignIn.isSignedIn();
+    this.setState(() {
+      isLoading = true;
+    });
+
     prefs = await SharedPreferences.getInstance();
+
+    isLoggedIn = await googleSignIn.isSignedIn();
     if (isLoggedIn) {
       Navigator.push(
         context,
         MaterialPageRoute(builder: (context) => MainScreen(currentUserId: prefs.getString('id'))),
       );
     }
+
+    this.setState(() {
+      isLoading = false;
+    });
   }
 
   Future<Null> handleSignIn() async {
     prefs = await SharedPreferences.getInstance();
 
-    if (prefs.getString('id') != null) {
-      Navigator.push(
-        context,
-        MaterialPageRoute(builder: (context) => MainScreen(currentUserId: prefs.getString('id'))),
-      );
-    } else {
+    this.setState(() {
+      isLoading = true;
+    });
+
+    GoogleSignInAccount googleUser = await googleSignIn.signIn();
+    GoogleSignInAuthentication googleAuth = await googleUser.authentication;
+    FirebaseUser firebaseUser = await firebaseAuth.signInWithGoogle(
+      accessToken: googleAuth.accessToken,
+      idToken: googleAuth.idToken,
+    );
+    if (firebaseUser != null) {
+      // Check is already sign up
+      final QuerySnapshot result =
+      await Firestore.instance.collection('users').where('id', isEqualTo: firebaseUser.uid).getDocuments();
+      final List<DocumentSnapshot> documents = result.documents;
+      if (documents.length == 0) {
+        // Update data to server if new user
+        Firestore.instance.collection('users').document(firebaseUser.uid).setData(
+            {'nickname': firebaseUser.displayName, 'photoUrl': firebaseUser.photoUrl, 'id': firebaseUser.uid});
+
+        // Write data to local
+        currentUser = firebaseUser;
+        await prefs.setString('id', currentUser.uid);
+        await prefs.setString('nickname', currentUser.displayName);
+        await prefs.setString('photoUrl', currentUser.photoUrl);
+      } else {
+        // Write data to local
+        await prefs.setString('id', documents[0]['id']);
+        await prefs.setString('nickname', documents[0]['nickname']);
+        await prefs.setString('photoUrl', documents[0]['photoUrl']);
+        await prefs.setString('aboutMe', documents[0]['aboutMe']);
+      }
+      Fluttertoast.showToast(msg: "Sign in success");
       this.setState(() {
-        isLoading = true;
+        isLoading = false;
       });
 
-      GoogleSignInAccount googleUser = await googleSignIn.signIn();
-      GoogleSignInAuthentication googleAuth = await googleUser.authentication;
-      FirebaseUser firebaseUser = await firebaseAuth.signInWithGoogle(
-        accessToken: googleAuth.accessToken,
-        idToken: googleAuth.idToken,
+      Navigator.push(
+        context,
+        MaterialPageRoute(
+            builder: (context) =>
+                MainScreen(
+                  currentUserId: firebaseUser.uid,
+                )),
       );
-      if (firebaseUser != null) {
-        // Check is already sign up
-        final QuerySnapshot result =
-            await Firestore.instance.collection('users').where('id', isEqualTo: firebaseUser.uid).getDocuments();
-        final List<DocumentSnapshot> documents = result.documents;
-        if (documents.length == 0) {
-          // Update data to server if new user
-          Firestore.instance.collection('users').document(firebaseUser.uid).setData(
-              {'nickname': firebaseUser.displayName, 'photoUrl': firebaseUser.photoUrl, 'id': firebaseUser.uid});
-
-          // Write data to local
-          currentUser = firebaseUser;
-          await prefs.setString('id', currentUser.uid);
-          await prefs.setString('nickname', currentUser.displayName);
-          await prefs.setString('photoUrl', currentUser.photoUrl);
-        } else {
-          // Write data to local
-          await prefs.setString('id', documents[0]['id']);
-          await prefs.setString('nickname', documents[0]['nickname']);
-          await prefs.setString('photoUrl', documents[0]['photoUrl']);
-          await prefs.setString('aboutMe', documents[0]['aboutMe']);
-        }
-        Fluttertoast.showToast(msg: "Sign in success");
-        this.setState(() {
-          isLoading = false;
-        });
-
-        Navigator.push(
-          context,
-          MaterialPageRoute(
-              builder: (context) => MainScreen(
-                    currentUserId: firebaseUser.uid,
-                  )),
-        );
-      } else {
-        Fluttertoast.showToast(msg: "Sign in fail");
-        this.setState(() {
-          isLoading = false;
-        });
-      }
+    } else {
+      Fluttertoast.showToast(msg: "Sign in fail");
+      this.setState(() {
+        isLoading = false;
+      });
     }
   }
 
@@ -152,13 +155,13 @@ class LoginScreenState extends State<LoginScreen> {
             Positioned(
               child: isLoading
                   ? Container(
-                      child: Center(
-                        child: CircularProgressIndicator(
-                          valueColor: AlwaysStoppedAnimation<Color>(themeColor),
-                        ),
-                      ),
-                      color: Colors.white.withOpacity(0.8),
-                    )
+                child: Center(
+                  child: CircularProgressIndicator(
+                    valueColor: AlwaysStoppedAnimation<Color>(themeColor),
+                  ),
+                ),
+                color: Colors.white.withOpacity(0.8),
+              )
                   : Container(),
             ),
           ],
