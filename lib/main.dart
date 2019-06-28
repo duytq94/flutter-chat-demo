61c3d8db86d5f607c3bcb388ecc19be9b7e4bd72
Duy Tran <tranquangduy7994@gmail.com>
2019-06-28 17:16:12 +0700

    update version cache network image and check null avatar

--- lib/main.dart
+++ lib/main.dart
@@ -131,21 +131,27 @@ class MainScreenState extends State<MainScreen> {
           child: Row(
             children: <Widget>[
               Material(
-                child: CachedNetworkImage(
-                  placeholder: (context, url) => Container(
-                        child: CircularProgressIndicator(
-                          strokeWidth: 1.0,
-                          valueColor: AlwaysStoppedAnimation<Color>(themeColor),
+                child: document['photoUrl'] != null
+                    ? CachedNetworkImage(
+                        placeholder: (context, url) => Container(
+                          child: CircularProgressIndicator(
+                            strokeWidth: 1.0,
+                            valueColor: AlwaysStoppedAnimation<Color>(themeColor),
+                          ),
+                          width: 50.0,
+                          height: 50.0,
+                          padding: EdgeInsets.all(15.0),
                         ),
+                        imageUrl: document['photoUrl'],
                         width: 50.0,
                         height: 50.0,
-                        padding: EdgeInsets.all(15.0),
+                        fit: BoxFit.cover,
+                      )
+                    : Icon(
+                        Icons.account_circle,
+                        size: 50.0,
+                        color: greyColor,
                       ),
-                  imageUrl: document['photoUrl'],
-                  width: 50.0,
-                  height: 50.0,
-                  fit: BoxFit.cover,
-                ),
                 borderRadius: BorderRadius.all(Radius.circular(25.0)),
                 clipBehavior: Clip.hardEdge,
               ),
