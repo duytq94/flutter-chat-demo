3825b3bebc5b0a8efd964fff89cb9b2c6d659490
duytq94 <tranquangduy7994@gmail.com>
2020-04-03 23:39:56 +0700

    temp update firebase_messaging to fix issue with flutter_local_notifications

--- ios/Flutter/flutter_export_environment.sh
+++ ios/Flutter/flutter_export_environment.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=/Users/tranquangduy/Documents/Flutter/flutter"
-export "FLUTTER_APPLICATION_PATH=/Users/tranquangduy/Documents/Flutter/flutter-chat-demo"
-export "FLUTTER_TARGET=/Users/tranquangduy/Documents/Flutter/flutter-chat-demo/lib/main.dart"
+export "FLUTTER_ROOT=/Users/lap01434/Documents/Flutter/flutter"
+export "FLUTTER_APPLICATION_PATH=/Users/lap01434/Documents/Flutter/flutter-chat-demo"
+export "FLUTTER_TARGET=/Users/lap01434/Documents/Flutter/flutter-chat-demo/lib/main.dart"
 export "FLUTTER_BUILD_DIR=build"
 export "SYMROOT=${SOURCE_ROOT}/../build/ios"
-export "FLUTTER_FRAMEWORK_DIR=/Users/tranquangduy/Documents/Flutter/flutter/bin/cache/artifacts/engine/ios"
+export "FLUTTER_FRAMEWORK_DIR=/Users/lap01434/Documents/Flutter/flutter/bin/cache/artifacts/engine/ios"
 export "FLUTTER_BUILD_NAME=1.0.0"
 export "FLUTTER_BUILD_NUMBER=1"
 export "TRACK_WIDGET_CREATION=true"
--- ios/Runner/AppDelegate.m
+++ ios/Runner/AppDelegate.m
@@ -5,6 +5,9 @@
 
 - (BOOL)application:(UIApplication *)application
     didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
+  if (@available(iOS 10.0, *)) {
+    [UNUserNotificationCenter currentNotificationCenter].delegate = (id<UNUserNotificationCenterDelegate>) self;
+  }
   [GeneratedPluginRegistrant registerWithRegistry:self];
   // Override point for customization after application launch.
   return [super application:application didFinishLaunchingWithOptions:launchOptions];
